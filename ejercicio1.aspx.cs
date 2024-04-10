e607e95e0ba3b271c892a4b5489b09c8babecce1
AceitunaGuerrera <tomasmunguia300821@hotmail.com>
2024-04-09 22:18:47 -0300

    agregue cosas a la tabla y cambie letras para que esten en negrita

--- ejercicio1.aspx.cs
+++ ejercicio1.aspx.cs
@@ -12,10 +12,10 @@ namespace PR3_TP2
         protected void btnGenerarTabla_Click(object sender, EventArgs e)
         {
             string tabla = "<table border='1'>";
-            tabla += "<tr> <td>Producto</td> <td>Cantidad</td> </tr>";
+            tabla += "<tr> <td><b>Producto</b></td> <td><b>Cantidad</b></td> </tr>";
             tabla += "<tr>";
-            tabla += "<td>" + "</td>";
-            tabla += "<td>" +"</td>";
+            tabla += "<td>" +txtIngrese1.Text+ "</td>";
+            tabla += "<td>" +txtCantidad1.Text+"</td>";
             tabla += "</tr>";
             tabla += "<tr>";
             tabla += "<td>" + "</td>";
